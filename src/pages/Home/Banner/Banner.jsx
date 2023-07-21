748487669c70305d8bc089ba1bd2bd64106e3d6d
moktubat <shadhinreja@gmail.com>
2023-07-22 03:14:28 +0600

    banner slider added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,50 +1,122 @@
-import { Player } from '@lottiefiles/react-lottie-player';
-import chatting from "../../../assets/chatting.json";
+import { Carousel, Typography, Button } from "@material-tailwind/react";
+import slider1 from "../../../assets/slider1.jpg";
+import slider2 from "../../../assets/slider2.jpg";
+import slider3 from "../../../assets/slider3.jpg";
 
 const Banner = () => {
   return (
-    <div className="bg-blend-overlay"
-      style={{
-        backgroundImage: "linear-gradient(to left, #a8edea 0%, #fed6e3 100%)",
-      }}
-    >
-      <div>
-      <div className="relative w-full h-full flex flex-col justify-center items-center overflow-hidden">
-        <div className="pt-6 sm:pt-12 lg:pt-24 w-full flex flex-col lg:flex-row">
-          <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
-            <div className="p-5 flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
-              <h1 className="py-5 text-2xl sm:text-5xl font-bold tracking-wide">
-              Have your <br /> best chat
-              </h1>
-              <h3 className="pb-2">
-              Fast, easy & unlimited messages, conference call services.
-              Discussing your moment and expressing your Thoughts.
-              </h3>
-              <p className="text-lg font-semibold tracking-wide">
-              Connect effortlessly with our user-friendly chat app. Stay in touch with friends and family through real-time messaging, emojis, and multimedia sharing. Download now to start chatting!
-              </p>
-
-              <div className="mt-10 flex flex-col sm:flex-row items-center">
-                
+    <>
+      <Carousel>
+        <div className="relative h-full w-full">
+          <img
+            src={slider1}
+            alt="image 1"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+            <div className="w-3/4 text-center md:w-2/4">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                The Beauty of Nature
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex justify-center gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
               </div>
             </div>
           </div>
-
-          <div className="relative w-full lg:w-1/2 flex flex-col justify-center items-center overflow-hidden">
-            <Player
-              autoplay
-              loop
-              src={chatting}
-              style={{ height: "300px", width: "300px" }}
-              className="w-2/3 lg:w-full rounded"
-            ></Player>
+        </div>
+        <div className="relative h-full w-full">
+          <img
+            src={slider2}
+            alt="image 2"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+            <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                The Beauty of Nature
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
-
-      </div>
-    </div>
-
-    </div>
+        <div className="relative h-full w-full">
+          <img
+            src={slider3}
+            alt="image 3"
+            className="h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+            <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              >
+                The Beauty of Nature
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-12 opacity-80"
+              >
+                It is not so much for its beauty that the forest makes a claim
+                upon men&apos;s hearts, as for that subtle something, that
+                quality of air that emanation from old trees, that so
+                wonderfully changes and renews a weary spirit.
+              </Typography>
+              <div className="flex gap-2">
+                <Button size="lg" color="white">
+                  Explore
+                </Button>
+                <Button size="lg" color="white" variant="text">
+                  Gallery
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
+    </>
   );
 };
 
